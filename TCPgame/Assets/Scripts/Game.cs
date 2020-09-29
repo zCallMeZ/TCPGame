0f87e8059f228349c4ea983ff06d98647facc419
Bryan <bryancrim@gmail.com>
2020-09-29 21:59:13 +0200

    Game Scripts added

---
+++ TCPgame/Assets/Scripts/Game.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private GameObject rock;
+    [SerializeField] private GameObject paper;
+    [SerializeField] private GameObject scissor;
+
+    [SerializeField] private GameObject win;
+    [SerializeField] private GameObject lose;
+    [SerializeField] private GameObject tied;
+
+    int player1;
+    int player2;
+
+    private void Update()
+    {
+        // Player 1 win
+        if (player1 == 0 && player2 == 2)
+        {
+            win.SetActive(true);
+            lose.SetActive(false);
+            tied.SetActive(false);
+        }
+        if (player1 == 2 && player2 == 1)
+        {
+            win.SetActive(true);
+            lose.SetActive(false);
+            tied.SetActive(false);
+        }
+        if (player1 == 1 && player2 == 0)
+        {
+            win.SetActive(true);
+            lose.SetActive(false);
+            tied.SetActive(false);
+        }
+
+        // Player 2 win
+        if (player2 == 0 && player1 == 2)
+        {
+            win.SetActive(true);
+            lose.SetActive(false);
+            tied.SetActive(false);
+        }
+        if (player2 == 2 && player1 == 1)
+        {
+            win.SetActive(true);
+            lose.SetActive(false);
+            tied.SetActive(false);
+        }
+        if (player2 == 1 && player1 == 0)
+        {
+            win.SetActive(true);
+            lose.SetActive(false);
+            tied.SetActive(false);
+        }
+        // Tied
+        if (player1 == 0 & player2 == 0)
+        {
+            win.SetActive(false);
+            lose.SetActive(false);
+            tied.SetActive(true);
+        }
+        if (player1 == 1 & player2 == 1)
+        {
+            win.SetActive(false);
+            lose.SetActive(false);
+            tied.SetActive(true);
+        }
+        if (player1 == 2 & player2 == 2)
+        {
+            win.SetActive(false);
+            lose.SetActive(false);
+            tied.SetActive(true);
+        }
+    }
+
+    private void RandomNmb()
+    {
+        player2 = Random.Range(0, 2);
+    }
+
+    public void Rock()
+    {
+        player1 = 0;
+        RandomNmb();
+    }
+
+    public void Paper()
+    {
+        player1 = 1; 
+        RandomNmb();
+    }
+
+    public void Scissor()
+    {
+        player1 = 2;
+        RandomNmb();
+    }
+}
--- TCPgame/Assets/Scripts/RockPaperScissor.cs
+++ TCPgame/Assets/Scripts/RockPaperScissor.cs
@@ -1,42 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class RockPaperScissor : MonoBehaviour
 {
-    [SerializeField] private TestMeshProUGUI txt;
-    [SerializeField] private TestMeshProUGUI result;
+    //public TestMeshProUGUI txt;
+    //public TestMeshProUGUI result;
 
-    void Random()
-    {
-        int x = Random.Range(0, 2);
-        if (x == 0) { txt.text = "Rock"; }
-        if (x == 1) { txt.text = "Paper"; }
-        if (x == 2) { txt.text = "Scissor"; }
-    }
+    //void Random()
+    //{
+    //    int x = Random.Range(0, 2);
+    //    if (x == 0) { txt.text = "Rock"; }
+    //    if (x == 1) { txt.text = "Paper"; }
+    //    if (x == 2) { txt.text = "Scissor"; }
+    //}
 
-    public void Rock()
-    {
-        Random();
-        if (txt.text == "Rock") { result.text = "Match tied"; }
-        if (txt.text == "Paper") { result.text = "You Lose"; }
-        if (txt.text == "Scissor") { result.text = "You WIN"; }
-    }
+    //public void Rock()
+    //{
+    //    Random();
+    //    if (txt.text == "Rock") { result.text = "Match tied"; }
+    //    if (txt.text == "Paper") { result.text = "You Lose"; }
+    //    if (txt.text == "Scissor") { result.text = "You WIN"; }
+    //}
 
-    public void Paper()
-    {
-        Random();
-        if (txt.text == "Paper") { result.text = "Match tied"; }
-        if (txt.text == "Scissor") { result.text = "You Lose"; }
-        if (txt.text == "Rock") { result.text = "You WIN"; }
-    }
+    //public void Paper()
+    //{
+    //    Random();
+    //    if (txt.text == "Paper") { result.text = "Match tied"; }
+    //    if (txt.text == "Scissor") { result.text = "You Lose"; }
+    //    if (txt.text == "Rock") { result.text = "You WIN"; }
+    //}
 
-    public void Scissor()
-    {
-        Random();
-        if (txt.text == "Scissor") { result.text = "Match tied"; }
-        if (txt.text == "Rock") { result.text = "You Lose"; }
-        if (txt.text == "Paper") { result.text = "You WIN"; }
-    }
+    //public void Scissor()
+    //{
+    //    Random();
+    //    if (txt.text == "Scissor") { result.text = "Match tied"; }
+    //    if (txt.text == "Rock") { result.text = "You Lose"; }
+    //    if (txt.text == "Paper") { result.text = "You WIN"; }
+    //}
 }
