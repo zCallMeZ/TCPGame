3106cfc37b00cf873d5732ff10050f923fd327c9
Bryan <bryancrim@gmail.com>
2020-09-29 22:37:07 +0200

    Game Scene added

--- TCPgame/Assets/Scripts/Game.cs
+++ TCPgame/Assets/Scripts/Game.cs
@@ -9,6 +9,10 @@ public class Game : MonoBehaviour
     [SerializeField] private GameObject paper;
     [SerializeField] private GameObject scissor;
 
+    [SerializeField] private GameObject rock2;
+    [SerializeField] private GameObject paper2;
+    [SerializeField] private GameObject scissor2;
+
     [SerializeField] private GameObject win;
     [SerializeField] private GameObject lose;
     [SerializeField] private GameObject tied;
@@ -41,20 +45,20 @@ public class Game : MonoBehaviour
         // Player 2 win
         if (player2 == 0 && player1 == 2)
         {
-            win.SetActive(true);
-            lose.SetActive(false);
+            win.SetActive(false);
+            lose.SetActive(true);
             tied.SetActive(false);
         }
         if (player2 == 2 && player1 == 1)
         {
-            win.SetActive(true);
-            lose.SetActive(false);
+            win.SetActive(false);
+            lose.SetActive(true);
             tied.SetActive(false);
         }
         if (player2 == 1 && player1 == 0)
         {
-            win.SetActive(true);
-            lose.SetActive(false);
+            win.SetActive(false);
+            lose.SetActive(true);
             tied.SetActive(false);
         }
         // Tied
@@ -81,23 +85,50 @@ public class Game : MonoBehaviour
     private void RandomNmb()
     {
         player2 = Random.Range(0, 2);
+        if (player2 == 0)
+        {
+            rock2.SetActive(true);
+            paper2.SetActive(false);
+            scissor2.SetActive(false);
+        }
+        if (player2 == 1)
+        {
+            rock2.SetActive(false);
+            paper2.SetActive(true);
+            scissor2.SetActive(false);
+        }
+        if (player2 == 2)
+        {
+            rock2.SetActive(false);
+            paper2.SetActive(false);
+            scissor2.SetActive(true);
+        }
     }
 
     public void Rock()
     {
         player1 = 0;
         RandomNmb();
+        rock.SetActive(true);
+        paper.SetActive(false);
+        scissor.SetActive(false);
     }
 
     public void Paper()
     {
         player1 = 1; 
         RandomNmb();
+        rock.SetActive(false);
+        paper.SetActive(true);
+        scissor.SetActive(false);
     }
 
     public void Scissor()
     {
         player1 = 2;
         RandomNmb();
+        rock.SetActive(false);
+        paper.SetActive(false);
+        scissor.SetActive(true);
     }
 }
